3b9b95a3f0a35ae77a9a98a1ab98f6f72c6a3f85
rohit2-tech <rohitrajgupta23@gmail.com>
2022-01-07 14:29:01 +0530

    refactored version

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
 //jshint esversion:6
 
-const express= require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
-const date= require(__dirname+"/date.js");
+const date = require(__dirname + "/date.js");
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
-let items=["LeetCode", "WedDevelopment", "CodeForces"] ;
-let workItems=[];
+const items = ["LeetCode", "WebDevelopment", "CodeForces"];
+const workItems = [];
 
-app.get("/",function(req,res){
-let day =date.getDay();
-  res.render("list",{
-    listTitle:day,
+app.get("/", function(req, res) {
+  let day = date.getDate();
+  res.render("list", {
+    listTitle: day,
     newListItems: items
   });
 });
 
-app.post("/",function(req,res){
+app.post("/", function(req, res) {
 
-  let item=req.body.newItem;
+  let item = req.body.newItem;
   console.log(req.body.list);
-  if(req.body.list=== "Work List"){
+  if (req.body.list === "Work List") {
     workItems.push(item);
     res.redirect("/work");
-  }
-  else{
+  } else {
     items.push(item);
-   res.redirect("/");
+    res.redirect("/");
   }
 
 });
 
-app.get("/work",function(req,res){
-  res.render("list",{
-    listTitle : "Work List",
-      newListItems: workItems
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
   });
 });
 
 
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("server started on port 3000");
 });
--- date.js
+++ date.js
@@ -1,26 +1,20 @@
 //jshint esversion:6
 
-module.exports.getDate = getDate;
-function getDate(){
-  let today = new Date();
-  let options = {
-    weekday : "long",
+exports.getDate = function() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
     day: "numeric",
     month: "long"
   };
-
-  let day =today.toLocaleDateString("en-us",options);
-return day;
+  return today.toLocaleDateString("en-us", options);
 }
 
-
-module.exports.getDay=getDay;
-function getDay(){
-  let today = new Date();
-  let options = {
-    weekday : "long",
+exports.getDay = function() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
   };
 
-  let day =today.toLocaleDateString("en-us",options);
-return day;
+  return today.toLocaleDateString("en-us", options);
 }
